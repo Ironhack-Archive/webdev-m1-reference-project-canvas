68a1aea3b24c6c827a04264985d8bedf1e70f2e3
Filipe Rainho <filipenrainho@gmail.com>
2018-07-19 19:31:06 +0200

    work on new restart screen

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,12 @@
 'use strict';
 
-function main () {
+function createHtml(html) {
+  var div = document.createElement("div");
+  div.innerHTML = html;
+  return div.children[0];
+};
+
+function main() {
   var canvasWidth = 800;
   var canvasHeight = 600;
 
@@ -11,47 +17,60 @@ function main () {
   var game = new Game(canvas.ctx, canvasWidth, canvasHeight, gameEnded, maxGoals);
 
   // Build Splash
-  function buildSplash () {
+  function buildSplash() {
     game.buildSplash();
     canvas.canvasElement.addEventListener('click', startGame);
   }
 
   // Desktroy Splash and build Gthis.ended = false;ame
-  function startGame () {
+  function startGame() {
     canvas.canvasElement.removeEventListener('click', startGame);
     game.destroySplash();
     buildGame();
   }
 
-  function handleKeyDown (event) {
+  function handleKeyDown(event) {
     game.handleKeyDown(event.key);
   }
 
-  function handleKeyUp (event) {
+  function handleKeyUp(event) {
     game.handleKeyUp(event.key);
   }
 
-  function buildGame () {
+  function buildGame() {
     game.ended = false;
     game.build();
     window.addEventListener('keydown', handleKeyDown);
     window.addEventListener('keyup', handleKeyUp);
   }
 
-  function gameEnded (winner) {
+  function gameEnded(winner) {
     destroyGame(winner);
   }
 
   // Destroy Game and build Game Over
-  function destroyGame (winner) {
-    buildGameOver(winner);
+  function destroyGame(winner) {
+    buildGameOverHTML(winner);
     window.removeEventListener('keydown', handleKeyDown);
     window.removeEventListener('keyup', handleKeyUp);
   }
 
-  function buildGameOver (winner) {
+  function buildGameOver(winner) {
     game.buildGameOver(winner);
     canvas.canvasElement.addEventListener('click', startGame);
+  }
+
+  function buildGameOverHTML(winner) {
+    canvas.remove();
+    var restartScreen = createHtml(`
+    <div>
+      <p>Game over</p>
+      <p>The player on the ${winner} won</p>
+      <button>Restart Game</button>
+    </div>
+    `)
+    document.body.appendChild(restartScreen);
+
   }
 
   buildSplash();
