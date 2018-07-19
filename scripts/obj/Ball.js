f7cae7334f50490c8fb873b0d7efe75ab6eaa88d
Filipe Rainho <filipenrainho@gmail.com>
2018-07-19 18:34:29 +0200

    Transform ball.js to es5

--- scripts/obj/Ball.js
+++ scripts/obj/Ball.js
@@ -1,43 +1,41 @@
 'use strict';
 
-class Ball {
-  constructor (ctx, canvasWidth, canvasHeight) {
-    this.ctx = ctx;
-    this.centerX = canvasWidth / 2;
-    this.centerY = canvasHeight / 2;
-    this.canvasWidth = canvasWidth;
-    this.canvasHeight = canvasHeight;
-    this.radius = 10;
-    this.directionV = 1;
-    this.directionH = 1;
-    this.speed = 2;
-  }
+function Ball(ctx, canvasWidth, canvasHeight) {
+  this.ctx = ctx;
+  this.centerX = canvasWidth / 2;
+  this.centerY = canvasHeight / 2;
+  this.canvasWidth = canvasWidth;
+  this.canvasHeight = canvasHeight;
+  this.radius = 10;
+  this.directionV = 1;
+  this.directionH = 1;
+  this.speed = 2;
+}
 
-  draw () {
-    this.ctx.fillStyle = 'white';
-    this.ctx.arc(this.centerX, this.centerY, this.radius, 0, 2 * Math.PI);
-    this.ctx.fill();
-  }
+Ball.prototype.draw = function () {
+  this.ctx.fillStyle = 'white';
+  this.ctx.arc(this.centerX, this.centerY, this.radius, 0, 2 * Math.PI);
+  this.ctx.fill();
+}
 
-  resetPosition () {
-    this.centerX = this.canvasWidth / 2;
-    this.centerY = this.canvasHeight / 2;
-  }
+Ball.prototype.resetPosition = function () {
+  this.centerX = this.canvasWidth / 2;
+  this.centerY = this.canvasHeight / 2;
+}
 
-  swapVertDirection () {
-    this.directionV = -this.directionV;
-  }
+Ball.prototype.swapVertDirection = function () {
+  this.directionV = -this.directionV;
+}
 
-  swapHoriDirection () {
-    this.directionH = -this.directionH;
-  }
+Ball.prototype.swapHoriDirection = function () {
+  this.directionH = -this.directionH;
+}
 
-  update () {
-    this.centerY = (this.directionV * this.speed) + this.centerY;
-    this.centerX = (this.directionH * this.speed) + this.centerX;
-  }
+Ball.prototype.update = function () {
+  this.centerY = (this.directionV * this.speed) + this.centerY;
+  this.centerX = (this.directionH * this.speed) + this.centerX;
+}
 
-  updateSpeed () {
-    this.speed++;
-  }
+Ball.prototype.updateSpeed = function () {
+  this.speed++;
 }
